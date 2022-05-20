5f2296e58a525aa2f00d413a49a3c7479c5750b2
CodexBox <smrutiranjan30@gmail.com>
2022-05-20 11:08:28 +0530

    marker interface program commit 5

--- src/smrutiranjan/practice/sample/NewClass.java
+++ src/smrutiranjan/practice/sample/NewClass.java
@@ -1,4 +1,8 @@
 package smrutiranjan.practice.sample;
 
 public class NewClass {
+    public static void main(String[] args) {
+        String s = "testing git hub :";
+        System.out.println(s);
+    }
 }
