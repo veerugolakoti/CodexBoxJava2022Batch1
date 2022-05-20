082a3395455a2583c9cd93866ca1d7f9b959329c
CodexBox <smrutiranjan30@gmail.com>
2022-05-20 13:35:15 +0530

    marker interface program commit 6

--- src/smrutiranjan/practice/markerinterface/MarkerMain.java
+++ src/smrutiranjan/practice/markerinterface/MarkerMain.java
@@ -1,6 +1,4 @@
 package smrutiranjan.practice.markerinterface;
-import smrutiranjan.practice.markerinterface.MarkerInterfaceEx;
-
 public class MarkerMain {
     public static void main(String[] args) throws CloneNotSupportedException {
         MarkerInterfaceEx mi = new MarkerInterfaceEx(5);
