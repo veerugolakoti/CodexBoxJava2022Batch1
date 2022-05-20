ba864e30cb79f0b97edfafbe070ca2fb96fe9c68
CodexBox <vasu.manginipelli@codexbox.com>
2022-05-20 12:54:17 +0530

    Initial Commit for java 2022 batch1 mark interfce

--- src/Vasusample/Java/MainInterfaceMarker.java
+++ src/Vasusample/Java/MainInterfaceMarker.java
@@ -5,9 +5,9 @@ public class MainInterfaceMarker {
         Interfacemarker I = new Interfacemarker("vaasu",1234);
         System.out.println(I.name);
         System.out.println(I.id);
-        I.n = "AAA";
-        I.i = 545;
-        System.out.println(I.i);
-        System.out.println(I.n);
+        I.name = "ravi";
+        I.id = 5456;
+        System.out.println(I.name);
+        System.out.println(I.id);
     }
 }
