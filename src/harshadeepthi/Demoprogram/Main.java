9676c9926e8fd8a8f013d66b63b42ef9fce05475
CodexBox <harshamulisetti56@gmail.com>
2022-05-20 16:58:38 +0530

    programs

--- src/harshadeepthi/Demoprogram/Main.java
+++ src/harshadeepthi/Demoprogram/Main.java
@@ -2,10 +2,14 @@ package harshadeepthi.Demoprogram;
 
 public class Main {
     public static void main(String[] args) throws CloneNotSupportedException {
-        Student s1 = new Student(101,"harsha", 22.0);
-        System.out.println(s1.studentname);
-        System.out.println(s1.Id);
-        System.out.println(s1.age);
+        Student s1 = new Student();
+        Student s2 = (Student) s1.getCloneMethod();
+        s2.age = 20;
+        s2.Id= 23;
+        s2.studentname= "Harsha";
+        System.out.println(s2.studentname);
+        System.out.println(s2.Id);
+        System.out.println(s2.age);
 
     }
 }
