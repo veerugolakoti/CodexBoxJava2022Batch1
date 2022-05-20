b26f9da7da53a7daf277715f8515443ee253635d
shiva1938 <shivashivam17@gmail.com>
2022-05-20 13:09:29 +0530

    Initial Commit for shiva encapsulation

--- src/Shiva/Encapsulation/Main.java
+++ src/Shiva/Encapsulation/Main.java
@@ -2,14 +2,14 @@ package Shiva.Encapsulation;
 
 public class Main {
     public static void main(String[] args) {
-        Student s1 = new Student();
-        s1.setName("Lenovo");
-        s1.setAdddress("Hyd");
-        s1.setMarks(78.34);
-        s1.setId(111);
-        System.out.println(s1.getId());
-        System.out.println(s1.getName());
-        System.out.println(s1.getAdddress());
-        System.out.println(s1.getMarks());
+        Student s2 = new Student();
+        s2.setName("Lenovo");
+        s2.setAdddress("Hyd");
+        s2.setMarks(78.34);
+        s2.setId(111);
+        System.out.println(s2.getId());
+        System.out.println(s2.getName());
+        System.out.println(s2.getAdddress());
+        System.out.println(s2.getMarks());
     }
 }
