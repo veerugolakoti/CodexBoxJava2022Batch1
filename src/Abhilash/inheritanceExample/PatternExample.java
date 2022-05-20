b909938c8e2a1ffeb2e0cef92603bc3bf2be6dc9
Abhilash <abhilashchintha3@gmail.com>
2022-05-20 18:29:15 +0530

    abhilash commits

---
+++ src/Abhilash/inheritanceExample/PatternExample.java
@@ -0,0 +1,46 @@
+package Abhilash.inheritanceExample;
+
+import java.util.Scanner;
+
+public class PatternExample {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println("");
+
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println("");
+
+        for(int i=0;i<n;i++){
+            for(int j=i ;j<n;j++){
+                if(i+j == 3){
+                    System.out.println(" ");
+                }
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println("");
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
+        System.out.println("");
+    }
+
+
+}
