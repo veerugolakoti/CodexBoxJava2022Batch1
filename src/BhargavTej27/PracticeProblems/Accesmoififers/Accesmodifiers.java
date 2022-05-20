156d4e734867fa22aa9a587b5e5ad8bdf6c065f3
bhargavasaiteja <saiteja.dwaram@codebox.com>
2022-05-20 10:46:57 +0530

    commit on 20.05.2022 @11am

---
+++ src/BhargavTej27/PracticeProblems/Accesmoififers/Accesmodifiers.java
@@ -0,0 +1,12 @@
+package BhargavTej27.PracticeProblems.Accesmoififers;
+
+class Accesmodifiers {
+     public void codexbox() {
+         System.out.println("Hello world!");
+     }
+
+//     public static void main(String[] args) {
+//         Accesmodifiers acces = new Accesmodifiers();
+//         acces.codexbox();
+//     }
+ }
--- src/junaid/sampleproject/interfaces/InterfacesImpl.java
+++ src/junaid/sampleproject/interfaces/InterfacesImpl.java
@@ -1,12 +1,13 @@
 package junaid.sampleproject.interfaces;
 
-public class InterfacesImpl implements MyNewInterface
+public class InterfacesImpl
+//        implements MyNewInterface
 {
-
-    @Override
-    public MyNewInterface getInterfaces()
-    {
-        System.out.println("I am inside the Get Instance Method");
-        return null;
-    }
+//
+//    @Override
+//    public MyNewInterface getInterfaces()
+//    {
+//        System.out.println("I am inside the Get Instance Method");
+//        return null;
+//    }
 }
