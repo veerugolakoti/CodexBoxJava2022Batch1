58c16bf51657f334cfaec15650cac8f6499f8f11
CodexBox <swathigajjela1998@gmail.com>
2022-05-20 12:19:00 +0530

    Initial commit my program 1..

---
+++ src/Swathi/Interface/MainMethod.java
@@ -0,0 +1,10 @@
+package Swathi.Interface;
+
+public class MainMethod {
+    public static void main(String[] args) {
+        InterfaceClass OBJ =new InterfaceClass();
+        OBJ.jump();
+        OBJ.walk();
+        OBJ.write();
+
+        }}
