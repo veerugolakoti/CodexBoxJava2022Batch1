e32df96ec349d7e13e07507377010fa45297708b
bhargavasaiteja <saiteja.dwaram@codebox.com>
2022-05-24 18:52:39 +0530

    exception handling by bhargav

---
+++ src/BhargavTej27/DailyTopicsProblems/ExceptionExamples/ExceptionHandling1.java
@@ -0,0 +1,23 @@
+package BhargavTej27.DailyTopicsProblems.ExceptionExamples;
+
+public class ExceptionHandling1 {
+    public static void main(String[] args) {
+        String st = null;
+        System.out.println(st.length());
+
+    }
+}
+//    public static float message(float a,float b){
+//        float i=a/b;
+//        return i;
+//    }
+////  public static int message2(int a,int b){
+////       int res = 0;
+////       try{
+////           res = message(a, b);
+////       }
+////       catch(NumberFormatException e){
+////           System.out.println("NumberFormatException is occured");
+////      }
+////       return res;
+////    }
