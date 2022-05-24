77f43fb2cea797dfd26cb117f75a2da7e713279b
CodexBox <nagarjunapdtm123>
2022-05-24 17:03:28 +0530

    user defined exception.

--- src/Nagarjuna/ExceptionHandling/AirthemeticException.java
+++ src/Nagarjuna/ExceptionHandling/AirthemeticException.java
@@ -1,18 +1,26 @@
 package Nagarjuna.ExceptionHandling;
 
 public class AirthemeticException {
-    public static void main(String[] args) {
-        try {
-            int a=100,b=0,c;
-            c=a/b;
-            System.out.println("result "+c);
-        }
-        catch (Exception e) {
-            System.out.println("my exception is Airthmetic exception "+e);
-        }
-    }
 
+
+    public static void main(String[] args) throws ArithmeticException{
+        try {
+            int a=20,b=0,c;
+            c=a/b;
+            System.out.println("print try block "+c);
+        }catch (ArithmeticException e) {
+            System.out.println("print catch block"+e.getMessage());
+
+        }
+
+    }
 }
+
+
+
+
+
+
        /* try {
             int[] a = {12, 4, 6, 7, 7};
             int i = a[6];
@@ -21,4 +29,13 @@ public class AirthemeticException {
         }
     }
 }*/
+       /* try {
+            int i = Integer.parseInt("nag");
+            System.out.println(i);
 
+        } catch (Exception e) {
+            System.out.println("number format exception "+e);
+        }
+    }
+}
+*/
