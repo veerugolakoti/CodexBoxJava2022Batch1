8e8b3afe3af595c75de7f258886d5b27991cf994
SASIGUNTURI <sasigunturi143@gmail.com>
2022-05-24 18:45:41 +0530

    commit to Exception handling

--- src/junaid/sampleproject/exceptionhandling/UserDefinedException.java
+++
@@ -1,9 +0,0 @@
-package junaid.sampleproject.exceptionhandling;
-
-public class UserDefinedException extends Exception
-{
-    public UserDefinedException(String str)
-    {
-        super(str);
-    }
-}
