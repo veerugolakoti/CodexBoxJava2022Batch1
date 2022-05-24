d8c05822d0464442ddecd5ccc6680d03cc7ba570
divya <dhivyakesavulu1999@gmail.com>
2022-05-24 18:39:24 +0530

    changes in main appliction

---
+++ src/Dhivya/sample/exceptionhandling/ArrayIndex.java
@@ -0,0 +1,27 @@
+package Dhivya.sample.exceptionhandling;
+
+import jdk.nashorn.internal.parser.Scanner;
+
+
+public class ArrayIndex {
+
+
+
+
+    public void getArray() {
+
+        int[] array=new int[100];
+        for (int i = 0; i < array.length; i++) {
+
+
+        }
+
+    }
+
+
+
+
+    }
+
+
+
