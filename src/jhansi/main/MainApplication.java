bb1c9c04902c83e388f9c4b15a5205963f3037f1
CodexBox <jhansi.gurubadu@codexbox.com>
2022-05-25 17:59:40 +0530

    Exception handling practice TicketDetails

--- src/jhansi/main/MainApplication.java
+++ src/jhansi/main/MainApplication.java
@@ -1,6 +1,10 @@
 package jhansi.main;
 
 import jhansi.ExceptionHandlingExamples.CheckedExample;
+import jhansi.collections.TicketDetails;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainApplication {
     public static void main(String[] args) {
@@ -24,8 +28,46 @@ public class MainApplication {
         //System.out.println("==============marker interface");
 
 
-        CheckedExample ckm = new CheckedExample();
-        ckm.method();
+      /*  CheckedExample ckm = new CheckedExample();
+        ckm.method();*/
+       // System.out.println("==========Exception Handling");
+
+        List<TicketDetails> list=new ArrayList<>();
+        TicketDetails tcs=new TicketDetails();
+        tcs.setName("jhansi");
+        tcs.setAge(23);
+        tcs.setAmount(780);
+        tcs.setAdress("kurnool");
+        tcs.setId(78963);
+
+        TicketDetails tcsa=new TicketDetails();
+        tcsa.setName("priyanka");
+        tcsa.setAge(20);
+        tcsa.setAmount(736);
+        tcsa.setAdress("hyderabad");
+        tcsa.setId(7896399);
+
+        TicketDetails tcsb=new TicketDetails();
+        tcsb.setName("sony");
+        tcsb.setAge(22);
+        tcsb.setAmount(799);
+        tcsb.setAdress("chennai");
+        tcsb.setId(789669);
+
+      list.add(tcs);
+      list.add(tcsa);
+      list.add(tcsb);
+
+        for ( TicketDetails  TicketDetails:list) {
+
+            System.out.println("Name:"+ TicketDetails.getName());
+            System.out.println("Age:"+ TicketDetails.getAge());
+            System.out.println("Id Number:"+ TicketDetails.getId());
+            System.out.println("Amount:"+ TicketDetails.getAmount());
+            System.out.println("Adress:"+ TicketDetails.getAdress());
+
+
+        }
 
     }
 }
