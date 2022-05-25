124899e91514734c7c95c8fb52d1b482ccf70d36
jadduleelaganesh <106057069+jadduleelaganesh@users.noreply.github.com>
2022-05-25 15:00:35 +0530

    Delete src/GANESH directory

--- src/GANESH/loops/DoWhileApplication.java
+++
@@ -1,9 +0,0 @@
-package GANESH.loops;
-
-public class DoWhileApplication {
-    public static void main(String[] args) {
-        int i = 10;
-      do {System.out.println("The value of i ="+i);
-          break;    } while (i<=11);
-}
-}
