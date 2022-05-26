1958a33136c1d607388d9121793cb4ce474c8c74
jadduleelaganesh <jadduleelaganesh@gmail.com>
2022-05-26 20:05:07 +0530

    Array List added

--- src/GANESH/sample/main/MainApplication.java
+++ src/GANESH/sample/main/MainApplication.java
@@ -1,19 +1,26 @@
 package ganesh.sample.main;
 
+import ganesh.sample.list.Arraylistexample;
 import ganesh.sample.stringoperations.ImmutableApplication;
 import ganesh.sample.stringoperations.mutableApplication;
 import sun.security.ec.point.ProjectivePoint;
 
+import java.util.ArrayList;
+
 public class MainApplication {
     public static void main(String[] args) {
-        //mutable application
+      /*  //mutable application
         mutableApplication gun = new mutableApplication();
         gun.m1();
 
         //Immutable application
         ImmutableApplication im = new ImmutableApplication();
         im.m2();
+*/
 
+        //Arraylist application
+        Arraylistexample alm = new Arraylistexample();
+        alm.m1();
     }
 
 }
