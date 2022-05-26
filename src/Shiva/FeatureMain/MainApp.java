3e9dd6da479d7e1e9a8eb6ae5d387fcb8baa3394
shiva1938 <shivashivam17@gmail.com>
2022-05-26 17:49:03 +0530

    duplicate number in array list

---
+++ src/Shiva/FeatureMain/MainApp.java
@@ -0,0 +1,14 @@
+package Shiva.FeatureMain;
+
+import Shiva.DuplicateElement.DuplicateNumber2;
+import Shiva.DuplicateElement.Duplicatenumber;
+
+public class MainApp {
+    public static void main(String[] args) {
+       /* Duplicatenumber dn = new Duplicatenumber();
+        dn.duplicateNumber();*/
+
+        DuplicateNumber2 dp2 = new DuplicateNumber2();
+        dp2.duplicateNumber2();
+    }
+}
