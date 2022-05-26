bf8b0998c54cb293a1fa3b63c5d0f5e891b5bcdf
CodexBox <vidya.darnacodexbox.com>
2022-05-26 10:13:34 +0530

    junit framework

--- src/vidya/frameworks/UnitExample.java
+++ src/vidya/frameworks/UnitExample.java
@@ -7,7 +7,7 @@ public class UnitExample {
     @Test
     public void checking() {
         JUnitexample jUnitexample = new JUnitexample();
-        Assert.assertEquals(32,jUnitexample.JUnitexample(10,12));
         Assert.assertEquals(22,jUnitexample.JUnitexample(10,12));
+
         }
     }
