5a8986e5c0271263e3ad2a86409e88410e4df84f
example1 <kalyankadamanchi5253@gmail.com>
2022-05-27 14:35:12 +0530

    List

--- src/Kalyan/Collection/Employee.java
+++ src/Kalyan/Collection/Employee.java
@@ -5,36 +5,31 @@ import java.util.List;
 
 public class Employee {
 
-   public void testExample() {
-       List<String> li = new ArrayList<>();
-       li.add("naresh");
-       li.add("ram");
-       li.add("kalyan");
-       li.add("teja");
-       li.add("kalyan");
-       li.add("vamshi");
-       li.add("sangam");
-       li.add("vamshi");
-       li.add("Kalyan");
-       li.add("chandan");
-       List<String> temp = new ArrayList<>();
-       List<Integer> getCount =  new ArrayList<>();
-       int count=0;
-        for (int i=0;i<li.size();i++) {
-            for (int j = i+1; j < li.size(); j++) {
-                if (li.get(i).equalsIgnoreCase(li.get(j))) {
-                    count++;
-                    if (temp.contains(li.get(j))){
-                        break;
-                    }else {
-                        temp.add(li.get(i));
-                        getCount.add(count);
-                    }
-                }
+    public void testExample() {
+        List<String> li = new ArrayList<>();
+        li.add("naresh");
+        li.add("ram");
+        li.add("kalyan");
+        li.add("teja");
+        li.add("kalyan");
+        li.add("vamshi");
+        li.add("sangam");
+        li.add("vamshi");
+        li.add("Kalyan");
+        li.add("chandan");
+        int count = 0;
+        String temp = "";
+        for (int i = 0; i < li.size(); i++) {
+            if (li.lastIndexOf(li.get(i)) !=i){
+                count++;
+                continue;
             }
+            System.out.println(temp+" "+count+"");
         }
-       System.out.println(temp+" dublicated "+getCount+" times");
-   }
     }
+}
+
+
+
 
 
--- src/Kalyan/main/MainProgram.java
+++ src/Kalyan/main/MainProgram.java
@@ -1,10 +1,13 @@
 package Kalyan.main;
-
+import Kalyan.Collection.DuplicateCharacter;
 import Kalyan.Collection.Employee;
 
 public class MainProgram {
     public static void main(String[] args) {
-        Employee arrayExample=new Employee();
-        arrayExample.testExample();
+       /*Employee arrayExample=new Employee();
+        arrayExample.testExample();*/
+        DuplicateCharacter dc=new DuplicateCharacter();
+        dc.character();
+
     }
 }
